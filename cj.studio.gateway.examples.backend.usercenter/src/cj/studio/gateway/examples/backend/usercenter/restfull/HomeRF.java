f72c67bb4ad115dc78651e32e0d3eb5f9e72b144
caroceanjofers <carocean.jofers@icloud.com>
2018-11-30 22:13:25 +0800

    examples-1.0.20181130.2213

---
+++ cj.studio.gateway.examples.backend.usercenter/src/cj/studio/gateway/examples/backend/usercenter/restfull/HomeRF.java
@@ -0,0 +1,19 @@
+package cj.studio.gateway.examples.backend.usercenter.restfull;
+
+import cj.studio.ecm.Scope;
+import cj.studio.ecm.annotation.CjService;
+import cj.studio.ecm.frame.Circuit;
+import cj.studio.ecm.frame.Frame;
+import cj.studio.ecm.graph.CircuitException;
+import cj.studio.gateway.socket.app.IGatewayAppSiteResource;
+import cj.studio.gateway.socket.app.IGatewayAppSiteWayWebView;
+
+@CjService(name = "/", scope = Scope.multiton)
+public class HomeRF implements IGatewayAppSiteWayWebView {
+
+	@Override
+	public void flow(Frame f, Circuit c, IGatewayAppSiteResource ctx) throws CircuitException {
+		c.content().writeBytes("xxxxx".getBytes());
+	}
+
+}
