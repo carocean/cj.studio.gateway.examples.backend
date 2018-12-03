932f13492837175190a0576067c39c13efab0bcf
cj <cj@192.168.1.108>
2018-12-04 00:02:46 +0800

    backends-1.0.20181204.0002

--- cj.studio.gateway.examples.backend.usercenter/src/cj/studio/gateway/examples/backend/usercenter/restfull/HomeRF.java
+++ cj.studio.gateway.examples.backend.usercenter/src/cj/studio/gateway/examples/backend/usercenter/restfull/HomeRF.java
@@ -1,5 +1,7 @@
 package cj.studio.gateway.examples.backend.usercenter.restfull;
 
+import java.util.Random;
+
 import cj.studio.ecm.Scope;
 import cj.studio.ecm.annotation.CjService;
 import cj.studio.ecm.frame.Circuit;
@@ -13,6 +15,12 @@ public class HomeRF implements IGatewayAppSiteWayWebView {
 
 	@Override
 	public void flow(Frame f, Circuit c, IGatewayAppSiteResource ctx) throws CircuitException {
+//		try {
+//			Thread.sleep(Integer.MAX_VALUE);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		c.content().writeBytes("xxxxx".getBytes());
 	}
 
